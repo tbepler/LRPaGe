279de04c7fd97f0577aa8878907f0aabca5217cf
jzou1115 <jzou1115@gmail.com>
2014-08-03 18:26:14 -0400

    Add generators for hashCode, toString, and equals

--- src/bepler/lrpage/code/generator/NodeGenerator.java
+++ src/bepler/lrpage/code/generator/NodeGenerator.java
@@ -409,6 +409,24 @@ public class NodeGenerator {
 			replace.body()._return(field);
 		}
 		
+		HashCodeGenerator h= new HashCodeGenerator();
+		StringGenerator s= new StringGenerator();
+		EqualsGenerator e= new EqualsGenerator();
+		for(JVar f:concreteNode.fields().values()){
+			h.appendField(f);
+			s.appendField(f);
+			e.appendField(f);
+		}
+		for(JMethod m:concreteNode.methods()){
+			h.appendMethod(JExpr.invoke(m));
+			s.appendMethod(JExpr.invoke(m));
+			e.appendMethod(JExpr.invoke(m));
+		}
+		
+		h.define(concreteNode);
+		s.define(concreteNode);
+		e.define(concreteNode);
+		
 		return concreteNode;
 		
 	}
